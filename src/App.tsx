6dc3971b73d6e9c57f4af3c5fbd8cd9bb582346f
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-06 14:45:08 +0000

    feat: register and login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Register } from './pages/register'
+import { LogIn } from './pages/login'
 
 function App() {
   return (
     <>
       <main>
         <Routes>
-          <Route path="/login" element={<Register />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<LogIn />} />
         </Routes>
       </main>
     </>
