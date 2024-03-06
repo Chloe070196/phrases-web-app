877069861b5e8b3c7eedaef4087793437ad3f55b
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-06 14:02:04 +0000

    feat: register page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Register } from './pages/register'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Welcome!</h1>
-      </div>
+      <main>
+        <Routes>
+          <Route path="/login" element={<Register />} />
+        </Routes>
+      </main>
     </>
   )
 }
