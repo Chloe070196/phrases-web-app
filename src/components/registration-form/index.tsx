53bc188c3664d14a56c1b9f78981c72675bf93bc
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-06 14:47:12 +0000

    feat: remove input label text

--- src/components/registration-form/index.tsx
+++ src/components/registration-form/index.tsx
@@ -1,17 +1,17 @@
 function RegistrationForm() {
     return (
         <form className="grid-rows-four">
-            <label>email
-                <input type="text" />
+            <label>
+                <input type="text" placeholder="your email address"/>
             </label>
-            <label>username
-                <input type="text" />
+            <label>
+                <input type="text" placeholder="a new username"/>
             </label>
-            <label>password
-                <input type="text" />
+            <label>
+                <input type="text" placeholder="a new password"/>
             </label>
-            <label>confirm password
-                <input type="text" />
+            <label>
+                <input type="text" placeholder="confirm your new password"/>
             </label>
             <button type="submit" className="submit-button">submit</button>
         </form>
