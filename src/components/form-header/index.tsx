611d346dae132cbb1c2714ff88cf864b595bf0c5
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-06 14:45:57 +0000

    feat: form header includes page link

--- src/components/form-header/index.tsx
+++ src/components/form-header/index.tsx
@@ -1,10 +1,12 @@
-function FormHeader( {formType, pageLinkText, message}: { formType: string, pageLinkText: string, message: string } ) {
+import { Link } from "react-router-dom"
+
+function FormHeader( {formType, linkText, pathTo, message}: { formType: string, linkText: string, pathTo: string, message: string } ) {
     return (
         <header className="form=header">
             <h2 className="form-header-item">Welcome!</h2>
-            <strong className="form-header-item">{ pageLinkText }</strong>
             <p className="form-header-item">{ message }</p>
             <h3>{ formType }</h3>
+            <Link to={pathTo}><strong className="link-to-form">{ linkText }</strong></Link>
         </header>
     )
 }
