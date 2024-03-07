9f8397011b64e675dff5ce3adfc034fac5796e6d
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 10:34:52 +0000

    feat: registration form functionality

--- src/components/registration-form/index.tsx
+++ src/components/registration-form/index.tsx
@@ -1,17 +1,45 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+import { registerNewUser } from "../../service/apiClient"
+const INITIAL_FORM = {
+    username: "",
+    email: "",
+    password: "",
+    repeatedPassword: "",
+}
+
 function RegistrationForm() {
+    const [form, setForm]= useState<{username: string, email: string, password: string, repeatedPassword: string} >(INITIAL_FORM)
+    const updateForm = (e: ChangeEvent<HTMLInputElement>) => {
+        setForm({...form, [e?.target?.name]: e?.target?.value })
+    }
+    const handleRegister = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (form.password !== form.repeatedPassword) {
+            return 
+        }
+        const newUser = {
+            username: form.username,
+            password: form.password,
+            email: form.email
+        }
+        const response = await registerNewUser(newUser)
+        console.log(response)
+        return response
+    }
+
     return (
-        <form className="grid-rows-four">
+        <form className="grid-rows-four" onSubmit={handleRegister}>
             <label>
-                <input type="text" placeholder="your email address"/>
+                <input onChange={updateForm} type="text" value={form.email} name="email" placeholder="your email address"/>
             </label>
             <label>
-                <input type="text" placeholder="a new username"/>
+                <input onChange={updateForm} type="text" value={form.username} name="username" placeholder="a new username"/>
             </label>
             <label>
-                <input type="text" placeholder="a new password"/>
+                <input onChange={updateForm} type="password" value={form.password} name="password" placeholder="a new password"/>
             </label>
             <label>
-                <input type="text" placeholder="confirm your new password"/>
+                <input onChange={updateForm} type="password" value={form.repeatedPassword} name="repeatedPassword" placeholder="confirm your new password"/>
             </label>
             <button type="submit" className="submit-button">submit</button>
         </form>
