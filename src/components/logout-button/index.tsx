977caf1d9a51c839f2cbc928a4eb1c338267ae21
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 19:00:46 +0000

    feat: log out button

---
+++ src/components/logout-button/index.tsx
@@ -0,0 +1,16 @@
+import { useContext } from "react"
+import { AuthContext } from "../context/auth"
+
+function LogOutButton() {
+    const {setUser} = useContext(AuthContext)
+    const handleLogOut = () => {
+        setUser(null)
+    }
+    return(
+        <>
+            <button onClick={handleLogOut}>Log out</button>
+        </>
+    )
+}
+
+export { LogOutButton }
