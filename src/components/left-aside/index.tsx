bedb7e5e72c833bf87741cc37120d97c77de083b
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 19:03:57 +0000

    feat: nav bar includes home and log out buttons

--- src/components/left-aside/index.tsx
+++ src/components/left-aside/index.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom"
 import "./style.css"
+import { LogOutButton } from "../logout-button"
 
 function LeftAside() {
     return(
         <>
         <aside>
             <nav className="grid-rows-four">
-                <Link className="page-link" to=""><h3>Learn</h3></Link>
-                <Link className="page-link" to=""><h3>Revise</h3></Link>
-                <Link className="page-link" to=""><h3>Practice</h3></Link>
-                <Link className="page-link" to=""><h3>Settings</h3></Link>
+                <Link className="page-link" to="/"><h3>Home</h3></Link>
+                <Link className="page-link" to="/learn"><h3>Learn</h3></Link>
+                <Link className="page-link" to="/practice"><h3>Revise</h3></Link>
+                <Link className="page-link" to="/revise"><h3>Practice</h3></Link>
+                <Link className="page-link" to="/settings"><h3>Settings</h3></Link>
             </nav>
+            <LogOutButton />
         </aside>
         </>
     )
