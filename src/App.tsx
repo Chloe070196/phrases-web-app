03ff64a1a0d3f6ca06de6de30445e1d10c323d41
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 18:48:41 +0000

    feat: implement pages and authprovider

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Register } from './pages/register'
 import { LogIn } from './pages/login'
-import { Home } from './pages/home'
+import { HomePage } from './pages/home'
+import { PracticePage } from './pages/practice'
+import { LearnPage } from './pages/learn'
+import { RevisePage } from './pages/revise'
+import { Settings } from './pages/settings'
+import { LeftAside } from './components/left-aside'
+import { AuthProvider} from './components/context/auth'
 
 function App() {
   return (
     <>
+      <AuthProvider>
+        <LeftAside />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/learn" element={<LearnPage />} />
+          <Route path="/practice" element={<PracticePage />} />
+          <Route path="/revise" element={<RevisePage />} />
+          <Route path="/settings" element={<Settings />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<LogIn />} />
         </Routes>
+      </AuthProvider>
     </>
   )
 }
