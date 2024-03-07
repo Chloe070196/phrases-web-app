e48a5f84333d27fa81187e28e4bbbe2f06f8f1e6
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 10:34:25 +0000

    feat: registerUser()

---
+++ src/service/apiClient.ts
@@ -0,0 +1,27 @@
+const rootUrl = import.meta.env.VITE_API_URL
+
+const request = async (endpoint: string, method: string, data: DataTransfer): Promise<Response> => {
+    const url = rootUrl + endpoint
+    const options = {
+        method: method, 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }  
+    return await fetch(url, options)
+}
+
+const post = async (endpoint: string, data: DataTransfer ): Promise<Response> => {
+    const method = "POST"
+    return await request(endpoint, method, data)
+} 
+
+
+const registerNewUser = async (data: DataTransfer): Promise<Response> => {
+    const response = await post('/register', data)
+    const responseData = await response.json()
+    return responseData
+}
+
+export { registerNewUser }
