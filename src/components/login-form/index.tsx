30da72f74fc843b3eab0d21e17a3d6a4483db497
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 11:18:53 +0000

    feat: login form functionality

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,11 +1,38 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+import { logIn } from "../../service/apiClient"
+import { useNavigate } from "react-router-dom"
+const INITIAL_FORM = {
+    email: "",
+    password: "",
+}
+
 function LogInForm() {
+    const [form, setForm] = useState<{ email: string, password: string }>(INITIAL_FORM)
+    const navigate = useNavigate()
+    const updateForm = (e: ChangeEvent<HTMLInputElement>) => {
+        setForm({ ...form, [e?.target?.name]: e?.target?.value })
+    }
+    const handleLogIn = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const providedCredentials = {
+            password: form.password,
+            email: form.email
+        }
+        try {
+            const response = await logIn(providedCredentials)
+            navigate("/")
+            return response
+        } catch (e) {
+            console.error('could not log in', e)
+        }
+    }
     return (
-        <form className="grid-rows-two">
+        <form className="grid-rows-four" onSubmit={handleLogIn}>
             <label>
-                <input type="text" placeholder="email@example.com"/>
+                <input onChange={updateForm} type="text" value={form.email} name="email" placeholder="your email address" />
             </label>
             <label>
-                <input type="text" placeholder="your password"/>
+                <input onChange={updateForm} type="password" value={form.password} name="password" placeholder="your password" />
             </label>
             <button type="submit" className="submit-button">submit</button>
         </form>
