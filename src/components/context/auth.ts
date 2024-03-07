a4deec7b6c4f39f11d0f8bb49637f7c6e4234561
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 18:47:42 +0000

    feat: authcontext and authprovider

---
+++ src/components/context/auth.ts
@@ -0,0 +1,21 @@
+import React, { createContext, useState, ReactNode } from "react";
+import type { AuthContextType, UserInContextType } from "../../types";
+
+const AuthContext = createContext<AuthContextType>({user: null, setUser: () => {}});
+
+const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<UserInContextType | null>(null);
+
+  const contextValue: AuthContextType = {
+    user,
+    setUser,
+  };
+
+  return React.createElement(
+    AuthContext.Provider,
+    { value: contextValue },
+    children
+  );
+};
+
+export { AuthContext, AuthProvider };
