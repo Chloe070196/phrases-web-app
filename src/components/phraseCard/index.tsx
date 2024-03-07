7afc835dd2e0c7862747a4a060471948d9781c36
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-07 13:55:39 +0000

    feat: basic card component

---
+++ src/components/phraseCard/index.tsx
@@ -0,0 +1,13 @@
+import type { PhraseType } from "../../types/types"
+
+function PhraseCard({ phrase }: { phrase: PhraseType }) {
+    return (
+        <li className="small-section-dimensions section-colors">
+            <h3>{phrase.content}</h3>
+            <p>{phrase.meaning}</p>
+            <p>{phrase.example}</p>
+        </li>
+    )
+}
+
+export { PhraseCard }
