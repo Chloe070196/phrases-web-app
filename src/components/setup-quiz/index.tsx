99c759de70dc2f82216d664b97f4e168f196f7f7
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-11 18:30:06 +0000

    fix: can start quiz

--- src/components/setup-quiz/index.tsx
+++ src/components/setup-quiz/index.tsx
@@ -1,16 +1,18 @@
 import { ChangeEvent, FormEvent, useState } from "react"
 import { getPhrases } from "../../service/apiClient"
 import { PhraseType } from "../../types"
+import { useNewPhrases } from "../../hooks/useNewPhrases"
 
-
-
-function SetUpQuiz({ handleStepCompletion, setNewPhrases }: { handleStepCompletion: () => void, setNewPhrases: React.Dispatch<React.SetStateAction<Array<PhraseType> | null>> }) {
+function SetUpQuiz({ handleStepCompletion, setNewPhrases, setCanProceed }: { handleStepCompletion: () => void, setNewPhrases: React.Dispatch<React.SetStateAction<Array<PhraseType> | null>>, setCanProceed: React.Dispatch<React.SetStateAction<boolean>> }) {
     const [phrasesNum, setPhrasesNum] = useState<number | null>(null)
+    const { onStart } = useNewPhrases()
 
     const handleStart = async (e: FormEvent<HTMLFormElement>) => {
         e?.preventDefault()
         const phraseList = await getPhrases(phrasesNum)
         setNewPhrases(phraseList)
+        onStart(phraseList)
+        setCanProceed(true)
         handleStepCompletion()
     }
     const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -21,11 +23,13 @@ function SetUpQuiz({ handleStepCompletion, setNewPhrases }: { handleStepCompleti
         <section className="quiz-setup-section">
             <h2>Learn new phrases</h2>
             <form onSubmit={handleStart}>
-                <select name="phrase_num" onChange={handleChange}>
-                    <option value="6">6</option>
-                    <option value="8">8</option>
-                    <option value="12">12</option>
-                </select>
+                <label>Number of new phrases : {' '}
+                    <select name="phrase_num" onChange={handleChange}>
+                        <option value="6">6</option>
+                        <option value="8">8</option>
+                        <option value="12">12</option>
+                    </select>
+                </label>
                 <button type="submit">Start</button>
             </form>
         </section>
