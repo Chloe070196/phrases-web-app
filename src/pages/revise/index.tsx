d2329e63ff1377879bfa88cdc32b79cf6d2a1222
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-12 16:37:38 +0000

    feat: recently learnt phrases on revise page

--- src/pages/revise/index.tsx
+++ src/pages/revise/index.tsx
@@ -1,10 +1,30 @@
+import { UserphraseList } from "../../components/userphraseList"
+import { getUserPhrases } from "../../service/apiClient"
+import { UserPhraseType } from "../../types"
+import { useCallback, useEffect, useState } from "react"
 
 function RevisePage() {
-    return (
-        <>
-            <main></main>
-        </>
-    )
+    const [userphraseList, setUserphraseList] = useState<Array<UserPhraseType> | null>(null)
+
+    const fetchUserPhrases = useCallback(async () => {
+        const phrases = await getUserPhrases()
+        setUserphraseList(phrases)
+    }, [])
+
+    useEffect(() => { fetchUserPhrases() }, [fetchUserPhrases])
+
+    if (userphraseList) {
+        return (
+            <>
+                <main>
+                    <h2>My phrases: </h2>
+                    <UserphraseList userphraseList={userphraseList}/>
+                </main>
+            </>
+        )
+    }
+    return (<></>)
 }
 
 export { RevisePage }
+
