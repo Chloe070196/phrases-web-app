1dc109a77bf33ff58a7fed8bbc13bfc4bc610797
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-12 18:31:21 +0000

    feat: basic sandbox layout

--- src/pages/practice/index.tsx
+++ src/pages/practice/index.tsx
@@ -2,7 +2,14 @@
 function PracticePage() {
     return (
         <>
-            <main></main>
+            <main>
+                <h2>Sandbox</h2>
+                <section className="centered-section-position">
+                    <h3>Practice using the phrases you've learnt!</h3>
+                    <textarea  className="medium-section-dimensions section-colors"/>
+                    <button className="bottom-right-button">Save</button>
+                </section>
+            </main>
         </>
     )
 }
